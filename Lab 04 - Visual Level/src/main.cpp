19cf7bc87347bfcf9ed3eecc2cc2bf9e2b2a8d11
njpatter <34224135+njpatter@users.noreply.github.com>
2026-09-23 08:28:36 -0500

    Update main.cpp

--- Lab 04 - Visual Level/src/main.cpp
+++ Lab 04 - Visual Level/src/main.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 #include <math.h>
 #include <Adafruit_BNO08x.h>
-
+void setReports();
 #define BNO08X_RESET -1
 
 Adafruit_BNO08x bno08x(BNO08X_RESET);
@@ -24,10 +24,20 @@ void setup(void) {
     }
   }
   Serial.println("BNO08x Found!");
+
+  setReports();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 }
 
+void setReports(void) {
+  Serial.println("Setting desired reports");
+  if (!bno08x.enableReport(SH2_ACCELEROMETER)) {
+    Serial.println("Could not enable accelerometer");
+  } else {
+    Serial.println("Set accelerometer report... success!");
+  }
+}
  
