be9b7da683c6756f038298df53d76dde2636d8aa
njpatter <34224135+njpatter@users.noreply.github.com>
2026-09-23 08:22:37 -0500

    Initial lab 04 code

---
+++ Lab 04 - Visual Level/src/main.cpp
@@ -0,0 +1,33 @@
+#include <Arduino.h>
+#include <math.h>
+#include <Adafruit_BNO08x.h>
+
+#define BNO08X_RESET -1
+
+Adafruit_BNO08x bno08x(BNO08X_RESET);
+sh2_SensorValue_t sensorValue;
+
+void setup(void) {
+  Serial.begin(115200);
+  while (!Serial)
+    delay(10); // will pause Zero, Leonardo, etc until serial console opens
+
+  Serial.println("Adafruit BNO08x test!");
+
+  // Try to initialize!
+  if (!bno08x.begin_I2C()) {
+    // if (!bno08x.begin_UART(&Serial1)) {  // Requires a device with > 300 byte
+    // UART buffer! if (!bno08x.begin_SPI(BNO08X_CS, BNO08X_INT)) {
+    Serial.println("Failed to find BNO08x chip");
+    while (1) {
+      delay(10);
+    }
+  }
+  Serial.println("BNO08x Found!");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
+ 
